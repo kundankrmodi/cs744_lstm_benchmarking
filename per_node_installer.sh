281877ab45bbc5227ab3c86f26ef062d71d67e56
sanchitideas <me@sanchit.net>
2019-11-27 16:45:10 -0600

    Installation script for nVidia drivers, CUDA, Tensorflow, Python
    To be run on each node

---
+++ per_node_installer.sh
@@ -0,0 +1,35 @@
+# Add NVIDIA package repositories
+wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/cuda-repo-ubuntu1804_10.0.130-1_amd64.deb
+sudo dpkg -i cuda-repo-ubuntu1804_10.0.130-1_amd64.deb
+sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/7fa2af80.pub
+sudo apt-get update
+wget http://developer.download.nvidia.com/compute/machine-learning/repos/ubuntu1804/x86_64/nvidia-machine-learning-repo-ubuntu1804_1.0.0-1_amd64.deb
+sudo apt install -q -y --force-yes ./nvidia-machine-learning-repo-ubuntu1804_1.0.0-1_amd64.deb
+sudo apt-get update
+
+# Install NVIDIA driver
+sudo apt-get install --no-install-recommends nvidia-driver-418
+# Reboot. Check that GPUs are visible using the command: nvidia-smi
+
+sudo apt-get install -q -y --force-yes libnvidia-compute-440
+sudo apt-get install -q -y --force-yes libnvidia-decode-440
+sudo apt-get install -q -y --force-yes libnvidia-encode-440
+sudo apt-get install -q -y --force-yes cuda-drivers
+sudo apt-get install -q -y --force-yes cuda-runtime-10-0
+
+# Install development and runtime libraries (~4GB)
+sudo apt-get install -q -y --force-yes --no-install-recommends \
+    cuda-10-0 \
+    libcudnn7=7.6.2.24-1+cuda10.0  \
+    libcudnn7-dev=7.6.2.24-1+cuda10.0
+
+
+# Install TensorRT. Requires that libcudnn7 is installed above.
+sudo apt-get install -y --no-install-recommends libnvinfer5=5.1.5-1+cuda10.0 \
+    libnvinfer-dev=5.1.5-1+cuda10.0
+
+sudo apt-get install python3-pip
+
+sudo -H pip3 install --upgrade pip
+
+sudo pip3 install tensorflow==1.14
